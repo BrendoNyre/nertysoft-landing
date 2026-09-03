5f604927d0d09ba3c9c4af53b57d045946372213
Brendo Nery de Oliveira <brendo.oliveira@ncinfo.com.br>
2026-09-03 16:02:35 -0300

    fix: desativar service workers legados de outros sistemas, adicionar favicon e suporte a subdominios

---
+++ sw.js
@@ -0,0 +1,24 @@
+﻿// Self-destroying Service Worker
+// Automatically unregisters any lingering service worker (e.g. from Nerty Finance) and purges old caches.
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => caches.delete(key)));
+    }).then(() => {
+      return self.registration.unregister();
+    }).then(() => {
+      return self.clients.matchAll({ type: 'window' });
+    }).then((clients) => {
+      clients.forEach((client) => {
+        if (client.url && 'navigate' in client) {
+          client.navigate(client.url);
+        }
+      });
+    })
+  );
+});
